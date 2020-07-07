544881d08fdfb311992d6a63cabdd2f4f0d187f0
KYH <toki666@naver.com>
2020-07-07 15:50:10 +0900

    add activitys

---
+++ app/src/main/java/com/kim9212/dateapplication/NewPlanActivity.java
@@ -0,0 +1,14 @@
+package com.kim9212.dateapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewPlanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_plan);
+    }
+}
---
+++ app/src/main/java/com/kim9212/dateapplication/SavedData.java
@@ -0,0 +1,14 @@
+package com.kim9212.dateapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SavedData extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_saved_data);
+    }
+}
