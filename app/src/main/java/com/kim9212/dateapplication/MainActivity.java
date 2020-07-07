2cb05ee309ba88513b56440a249dc445d1b51c87
KYH <toki666@naver.com>
2020-07-07 13:08:53 +0900

    Initial commit

---
+++ app/src/main/java/com/kim9212/dateapplication/MainActivity.java
@@ -0,0 +1,18 @@
+package com.kim9212.dateapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void clickBtn(View view) {
+    }
+}
