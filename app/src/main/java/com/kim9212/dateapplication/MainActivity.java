a314fd0cf37402d6af93ec6fb1af56e1d72cd540
KYH <toki666@naver.com>
2020-07-09 15:42:54 +0900

    kakao map

--- app/src/main/java/com/kim9212/dateapplication/MainActivity.java
+++ app/src/main/java/com/kim9212/dateapplication/MainActivity.java
@@ -2,9 +2,17 @@ package com.kim9212.dateapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,9 +23,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String a= getSigneture(this);
+        Log.i("Tag",a);
+
     }
 
+    public static String getSigneture(Context context){
+        PackageManager pm = context.getPackageManager();
+        try{
+            PackageInfo packageInfo = pm.getPackageInfo(context.getPackageName(), PackageManager.GET_SIGNATURES);
 
+            for(int i = 0; i < packageInfo.signatures.length; i++){
+                Signature signature = packageInfo.signatures[i];
+                try {
+                    MessageDigest md = MessageDigest.getInstance("SHA");
+                    md.update(signature.toByteArray());
+                    return Base64.encodeToString(md.digest(), Base64.NO_WRAP);
+                } catch (NoSuchAlgorithmException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+        }catch(PackageManager.NameNotFoundException e){
+            e.printStackTrace();
+        }
+        return null;
+    }
 
     public void startnewplan(View view) {
     }
--- app/src/main/java/com/kim9212/dateapplication/MapActivity.java
+++ app/src/main/java/com/kim9212/dateapplication/MapActivity.java
@@ -3,12 +3,19 @@ package com.kim9212.dateapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.ViewGroup;
+
+import net.daum.android.map.MapView;
 
 public class MapActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_map);
+        setContentView(R.layout.activity_main);
+
+        MapView mapView = new MapView(this);
+        ViewGroup mapViewContainer = findViewById(R.id.map_view);
+        mapViewContainer.addView(mapView);
     }
 }
