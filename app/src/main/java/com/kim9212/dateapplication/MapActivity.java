61b8b4d5910256ae14b54d59a3957d12f4867446
KYH <toki666@naver.com>
2020-07-24 10:53:38 +0900

    add button

--- app/src/main/java/com/kim9212/dateapplication/MapActivity.java
+++ app/src/main/java/com/kim9212/dateapplication/MapActivity.java
@@ -1,10 +1,14 @@
 package com.kim9212.dateapplication;
+
 import android.Manifest;
 import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.LocationManager;
@@ -16,6 +20,7 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Handler;
 import android.telephony.SmsManager;
 import android.util.Log;
 import android.view.KeyEvent;
@@ -29,6 +34,7 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -41,11 +47,13 @@ import org.w3c.dom.Text;
 import java.util.List;
 import java.util.Locale;
 
-public class MapActivity extends AppCompatActivity  {
+public class MapActivity extends AppCompatActivity {
 
-    private EditText et,et1;
+    private EditText et, et1;
     private WebView wv;
     private Button bt;
+    private ProgressDialog progressDialog;
+
 
     @Override
     protected void onStart() {
@@ -74,14 +82,27 @@ public class MapActivity extends AppCompatActivity  {
         wv = findViewById(R.id.wv);
         bt = findViewById(R.id.bt);
 
-        wv.setWebViewClient(new WebViewClient() {
-            @Override
-            public void onPageFinished(WebView view, String url) {
+        progressDialog= new ProgressDialog(this);
 
-//                Toast.makeText(MainActivity.this, "로딩 끝", Toast.LENGTH_SHORT).show();
+
+        wv.setWebViewClient(new WebViewClient() {
+            @Override //로딩 감지
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+
+                progressDialog.setMessage("행복해주세요");
+                progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+                progressDialog.show();
 
             }
+
+            @Override //로딩끝날때
+            public void onPageFinished(WebView view, String url) {
+
+                progressDialog.dismiss();
+            }
         });
+
         et.setOnKeyListener(new View.OnKeyListener() { //enter먹게하는거
             public boolean onKey(View v, int keyCode, KeyEvent event) {
 
@@ -113,6 +134,7 @@ public class MapActivity extends AppCompatActivity  {
             }
         });
     }
+
     public void loadsite(View view) {
         String address = et.getText().toString();
         if (!address.startsWith("https://")) {
@@ -120,6 +142,7 @@ public class MapActivity extends AppCompatActivity  {
         }
         wv.loadUrl(address);
     }
+
     public void onBackPressed() {
         if (wv.canGoBack()) {
             wv.goBack();
@@ -129,30 +152,31 @@ public class MapActivity extends AppCompatActivity  {
 
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu, menu);
-        return true;
-    }
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.action_back:
-                if (wv.canGoBack()) {
-                    wv.goBack();
-                }
-                return true;
-            case R.id.action_forward:
-                if (wv.canGoForward()) {
-                    wv.goForward();
-                }
-                return true;
-            case R.id.action_refresh:
-                wv.reload();
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+    //
+//    @Override //벡버튼이랑 새로고침버튼 나오게 하는것 지금은 액션바를 없에면서 못씀. 나중에 쓰고싶으면 쓰기
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        getMenuInflater().inflate(R.menu.menu, menu);
+//        return true;
+//    }
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        switch (item.getItemId()) {
+//            case R.id.action_back:
+//                if (wv.canGoBack()) {
+//                    wv.goBack();
+//                }
+//                return true;
+//            case R.id.action_forward:
+//                if (wv.canGoForward()) {
+//                    wv.goForward();
+//                }
+//                return true;
+//            case R.id.action_refresh:
+//                wv.reload();
+//                return true;
+//        }
+//        return super.onOptionsItemSelected(item);
+//    }
     public void clickbtn(View view) {
 
         try {
@@ -161,14 +185,14 @@ public class MapActivity extends AppCompatActivity  {
             Uri uri = Uri.parse("kakaomap://search?q=" + s + "맛집" + "&p=37.537229,127.005515");
             Intent intent = new Intent(Intent.ACTION_VIEW, uri);
             startActivity(intent);
-        }catch ( Exception e){
+        } catch (Exception e) {
             throw e;
         }
     }
 
 
     public void clickbtn2(View view) {
-        Intent intent = new Intent(this,MovieListActivity.class);
+        Intent intent = new Intent(this, MovieListActivity.class);
         startActivity(intent);
 
 
@@ -181,7 +205,7 @@ public class MapActivity extends AppCompatActivity  {
             Uri uri = Uri.parse("kakaomap://search?q=" + s + "영화관" + "&p=37.537229,127.005515");
             Intent intent = new Intent(Intent.ACTION_VIEW, uri);
             startActivity(intent);
-        }catch ( Exception e){
+        } catch (Exception e) {
             throw e;
         }
 
